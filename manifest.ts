dea34a56b2ffedc97d95f2130189cb2a0c805955
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2025-04-17 12:46:41 +0100

    wrote down some ideas of what I want to add

--- manifest.ts
+++ manifest.ts
@@ -16,3 +16,18 @@ export default Manifest({
   outgoingDomains: [],
   botScopes: ["commands", "chat:write", "chat:write.public"],
 });
+
+// TODO
+// button in shout outs channel that triggers this
+// trigger to update slack channel and principle of the month
+//  - can I restrict who calls this?
+// trigger to get list of shoutouts for reporting
+//  - can I restrict who calls this?
+//      - could hard code user id and check that but seems like a bad idea
+//      - looks like you can check if a user is an admin!!
+//  - select start and end date
+//  - return
+//    - top 3 givers
+//    - top 3 receivers
+//    - top value
+
