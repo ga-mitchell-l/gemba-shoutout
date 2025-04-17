7cca88d8a74033751a287fe7f6a15e4b79c967de
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2025-04-17 12:47:11 +0100

    added data store for recording the shout outs

---
+++ datastores/shout_outs.ts
@@ -0,0 +1,34 @@
+import { DefineDatastore, Schema } from "deno-slack-sdk/mod.ts";
+
+/**
+ * Datastores are a Slack-hosted location to store
+ * and retrieve data for your app.
+ * https://api.slack.com/automation/datastores
+ */
+export default DefineDatastore({
+  name: "shout_outs",
+  primary_key: "id",
+  attributes: {
+    id: {
+      type: Schema.types.string,
+    },
+    receiving_gemban: {
+        type: Schema.slack.types.user_id,
+    },
+    shouting_gemban: {
+        type: Schema.slack.types.user_id,
+    },
+    guiding_principle: {
+        type: Schema.types.string,
+    },
+    shout_out_message: {
+        type: Schema.types.string
+    },
+    timestamp: {
+        type: Schema.slack.types.timestamp
+    },
+    points: {
+        type: Schema.types.integer
+    }
+  },
+});
