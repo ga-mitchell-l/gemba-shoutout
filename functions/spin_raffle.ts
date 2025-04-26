9fe52079702906308af169beb5e4cd3471ed2a14
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2025-04-27 00:25:01 +0100

    made a start on the raffle message

--- functions/spin_raffle.ts
+++ functions/spin_raffle.ts
@@ -9,57 +9,136 @@ export const SpinRaffleFunction = DefineFunction({
       event_timestamp: {
         type: Schema.slack.types.timestamp,
       },
+      channel_id: {
+        type: Schema.slack.types.channel_id,
+      },
     },
     required: [
       "event_timestamp",
+      "channel_id",
     ],
   },
   output_parameters: {
-    properties: {
-      user_id: {
-        type: Schema.slack.types.user_id,
-      },
-    },
-    required: ["user_id"],
+    properties: {},
+    required: [],
   },
 });
 
 export default SlackFunction(
   SpinRaffleFunction,
-  ({ inputs }) => {
+  async ({ inputs, client }) => {
     const {
       event_timestamp,
+      channel_id,
     } = inputs;
 
     const raffle_date = new Date(event_timestamp * 1000);
-    const month_start = new Date(
-      raffle_date.getFullYear(),
-      raffle_date.getMonth(),
-      1,
+
+    // if (raffle_date.getDate()!==1) {
+    //     return
+    // }
+    const { month_start_timestamp, month_end_timestamp } = getMonthTimeStamps(
+      raffle_date,
     );
 
-    let month_end = new Date();
+    const result = await client.apps.datastore.query({
+      datastore: "ShoutOutDataStore",
+      expression: "#timestamp between :time_start AND :time_end",
+      expression_attributes: { "#timestamp": "timestamp" },
+      expression_values: {
+        ":time_start": month_start_timestamp,
+        ":time_end": month_end_timestamp,
+      },
+    });
 
-    if (raffle_date.getMonth() == 11) {
-      month_end = new Date(raffle_date.getFullYear() + 1, 0, 1);
-    } else {
-      month_end = new Date(
-        raffle_date.getFullYear(),
-        raffle_date.getMonth() + 1,
-        1,
-      );
+    const raffle_entries: string[] = [];
+    const shouting_gembans = new Set<string>();
+    const receiving_gembans = new Set<string>();
+
+    result.items.forEach((shoutout) => {
+      for (let i = 0; i < shoutout.points; i++) {
+        raffle_entries.push(shoutout.receiving_gemban);
+        receiving_gembans.add(shoutout.receiving_gemban);
+
+        raffle_entries.push(shoutout.shouting_gemban);
+        shouting_gembans.add(shoutout.shouting_gemban);
+      }
+    });
+
+    const winnerIndex = Math.floor(Math.random() * (raffle_entries.length - 1));
+    const winner_user_id = raffle_entries[winnerIndex];
+    const month = raffle_date.toLocaleDateString("en-GB", { month: "long" });
+    const shouting_count = shouting_gembans.size;
+    const receiving_count = receiving_gembans.size;
+    const nextMonth = new Date(
+      raffle_date.getFullYear(),
+      raffle_date.getMonth() + 1,
+      1,
+    ).toLocaleDateString("en-GB", { month: "long" });
+
+    let raffle_message = `Hi Gemba! *${month}* has brought us `;
+    raffle_message += `*${shouting_count}* Gembans shouting out - `;
+    raffle_message +=
+      `with a total of *${receiving_count}* of you receiving shoutouts!\n`;
+    raffle_message +=
+      `And remember, you get double points if your shoutout is tagged with one of `;
+    raffle_message +=
+      `<https://gembaadvantage.atlassian.net/wiki/spaces/BA/pages/1011515428/Our+Guiding+Principles|our Guiding Principles>, `;
+    raffle_message +=
+      `and triple shoutout points if it's the guiding principle of the month! :martial_arts_uniform: :rocket: :handshake::skin-tone-3: :first_place_medal:\n`;
+    raffle_message += `\n`;
+    raffle_message +=
+      `:drum_with_drumsticks: :drum_with_drumsticks: :drum_with_drumsticks:\n`;
+    raffle_message +=
+      `Our winner for ${month} is <@${winner_user_id}> :partying_face::bongo_blob:\n`;
+    raffle_message += `\n`;
+    raffle_message += `${nextMonth}'s guiding principle is: *TODO*\n`;
+    raffle_message +=
+      `Don’t forget to <https://gembaadvantage.atlassian.net/servicedesk/customer/portal/14|raise a ticket> `;
+    raffle_message +=
+      `and nominate people for Small Awards too! We want to continue hearing and celebrating you! `;
+    raffle_message += `:admission_tickets: :admission_tickets:`;
+
+    // could do something cool selecting a random few emojis from a list
+
+    // would be nice to include the users photo
+    // need to call api to get user info
+    // also pass blocks rather than text into post message
+    try {
+      const result = await client.chat.postMessage({
+        channel: channel_id,
+        text: raffle_message,
+      });
+
+      console.log(result);
+    } catch (error) {
+      console.error(error);
     }
 
-    let month_end_timestamp = Math.floor(month_end.getTime() / 1000)
-    let month_start_timestamp = Math.floor(month_start.getTime() / 1000)
-
-    // const result = await client.apps.datastore.query({
-    //     datastore: "ShoutOutDataStore",
-    //     expression: "#timestamp between :time_start AND :time_end",
-    //     expression_attributes: { "#message_term": "message" },
-    //     expression_values: { ":message": "timesheet" },
-    // });
-
-    return { outputs: { user_id: "U05KGP8DYV8" } };
+    return { outputs: {} };
   },
 );
+
+function getMonthTimeStamps(raffle_date: Date) {
+  const month_start = new Date(
+    raffle_date.getFullYear(),
+    raffle_date.getMonth(),
+    1,
+  );
+
+  let month_end = new Date();
+
+  if (raffle_date.getMonth() == 11) {
+    month_end = new Date(raffle_date.getFullYear() + 1, 0, 1);
+  } else {
+    month_end = new Date(
+      raffle_date.getFullYear(),
+      raffle_date.getMonth() + 1,
+      1,
+    );
+  }
+
+  const month_end_timestamp = Math.floor(month_end.getTime() / 1000);
+  const month_start_timestamp = Math.floor(month_start.getTime() / 1000);
+  return { month_start_timestamp, month_end_timestamp };
+}
--- workflows/raffle.ts
+++ workflows/raffle.ts
@@ -1,4 +1,6 @@
 import { DefineWorkflow, Schema } from "deno-slack-sdk/mod.ts";
+import { SpinRaffleFunction } from "../functions/spin_raffle.ts";
+import { GetConfigFunction } from "../functions/get_config.ts";
 
 const RaffleWorkflow = DefineWorkflow({
   callback_id: "raffle_workflow",
@@ -9,10 +11,15 @@ const RaffleWorkflow = DefineWorkflow({
       user_id: { type: Schema.types.string },
       event_timestamp: { type: Schema.slack.types.timestamp },
     },
-    required: [],
+    required: ["event_timestamp", "user_id"],
   },
 });
 
-console.log("raffle work flow triggering")
+const config = RaffleWorkflow.addStep(GetConfigFunction, {});
+
+RaffleWorkflow.addStep(SpinRaffleFunction, {
+  event_timestamp: RaffleWorkflow.inputs.event_timestamp,
+  channel_id: config.outputs.channel_id,
+});
 
 export { RaffleWorkflow };
